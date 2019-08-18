2b6b22f15637c8d64eaec4247717201357ea0457
Your Name <you@example.com>
2019-08-18 14:45:22 -0700

    change url

--- fridgedoor/urls.py
+++ fridgedoor/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('words/', views.wordListJSON, name='wordListJSON'),
+	path('words', views.wordListJSON, name='wordListJSON'),
 	path('<slug:door>', views.door, name='door'),
 	path('<slug:door>/magnets', views.magnetsJSON, name='magnetsJSON'),
 	path('', views.index, name='index')
