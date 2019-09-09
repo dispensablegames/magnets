3f2b6bebc9d58db3ab0d60d9fb6f27abec63b1d0
Your Name <you@example.com>
2019-09-08 22:29:03 -0400

    add host to settings

--- magnet/settings.py
+++ magnet/settings.py
@@ -26,7 +26,7 @@ with open(os.path.join(BASE_DIR, 'key.txt')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'poetrymag.net']
 
 
 # Application definition
